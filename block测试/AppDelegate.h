6a49c8adfc3855cfe2101834d766bc4310ca5e4d
ariel <xdanealine@gmail.com>
2016-01-05 16:18:41 +0800

    Initial Commit

---
+++ block测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  block测试
+//
+//  Created by csip on 16/1/5.
+//  Copyright © 2016年 ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ block测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  block测试
+//
+//  Created by csip on 16/1/5.
+//  Copyright © 2016年 ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
